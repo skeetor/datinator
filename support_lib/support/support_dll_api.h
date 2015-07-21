f359c4a5f328cbc21bcd9c0bf196763d3dde08da
skeetor <sparhawk@gmx.at>
2015-07-21 20:42:27 +0200

    Config.h added for debugging exported symbols.

--- support_lib/support/support_dll_api.h
+++ support_lib/support/support_dll_api.h
@@ -11,31 +11,43 @@
 #include <windows.h>
 #endif // _WIN32
 
+#include <support/config.h>
+
 // When building the DLL library, BUILD_SUPPORT_DLL must be defined
 // in order to export the symbols.
 #ifdef BUILD_SUPPORT_STATIC
 	#define SUPPORT_DLL_EXPORT
-	#warning SUPPORT_STATIC
+	#ifdef DEBUG_EXPORT_SYMBOLS
+		#warning SUPPORT_STATIC
+	#endif
 #else
 	#ifdef BUILD_SUPPORT_DLL		// Build the DLL itself
         #ifdef _WIN32
             #define SUPPORT_DLL_EXPORT __declspec(dllexport)
-			#warning SUPPORT_SHARED EXPORT WIN32
+			#ifdef DEBUG_EXPORT_SYMBOLS
+				#warning SUPPORT_SHARED EXPORT WIN32
+			#endif
         #endif // _WIN32
 
         #ifdef _LINUX
             #define SUPPORT_DLL_EXPORT __attribute__ ((visibility ("default")))
-			#warning SUPPORT_SHARED EXPORT LINUX
+			#ifdef DEBUG_EXPORT_SYMBOLS
+				#warning SUPPORT_SHARED EXPORT LINUX
+			#endif
         #endif // _LINUX
 	#else							// Make use of the DLL
         #ifdef _WIN32
             #define SUPPORT_DLL_EXPORT __declspec(dllimport)
-			#warning SUPPORT_SHARED IMPORT WIN32
+			#ifdef DEBUG_EXPORT_SYMBOLS
+				#warning SUPPORT_SHARED IMPORT WIN32
+			#endif
         #endif // _WIN32
 
         #ifdef _LINUX
             #define SUPPORT_DLL_EXPORT
-			#warning SUPPORT_SHARED IMPORT LINUX
+			#ifdef DEBUG_EXPORT_SYMBOLS
+				#warning SUPPORT_SHARED IMPORT LINUX
+			#endif
         #endif // _LINUX
 	#endif
 #endif
--- support_lib/support_qt/support_qt_dll_api.h
+++ support_lib/support_qt/support_qt_dll_api.h
@@ -11,32 +11,44 @@
 #include <windows.h>
 #endif // _WIN32
 
+#include <support/config.h>
+
 // When building the DLL library, BUILD_SUPPORT_QT_DLL must be defined
 // in order to export the symbols.
 
 #ifdef BUILD_SUPPORT_QT_STATIC
 	#define SUPPORT_QT_DLL_EXPORT
-	#warning SUPPORT_QT_STATIC
+	#ifdef DEBUG_EXPORT_SYMBOLS
+		#warning SUPPORT_QT_STATIC
+	#endif
 #else
 	#ifdef BUILD_SUPPORT_QT_DLL
         #ifdef _WIN32
             #define SUPPORT_QT_DLL_EXPORT __declspec(dllexport)
-			#warning SUPPORT_QT_SHARED EXPORT WIN32
+			#ifdef DEBUG_EXPORT_SYMBOLS
+				#warning SUPPORT_QT_SHARED EXPORT WIN32
+			#endif
         #endif // _WIN32
 
         #ifdef _LINUX
             #define SUPPORT_QT_DLL_EXPORT __attribute__ ((visibility ("default")))
-			#warning SUPPORT_QT_SHARED EXPORT LINUX
+			#ifdef DEBUG_EXPORT_SYMBOLS
+				#warning SUPPORT_QT_SHARED EXPORT LINUX
+			#endif
         #endif // _LINUX
 	#else
         #ifdef _WIN32
             #define SUPPORT_QT_DLL_EXPORT __declspec(dllimport)
-			#warning SUPPORT_QT_SHARED IMPORT WIN32
+			#ifdef DEBUG_EXPORT_SYMBOLS
+				#warning SUPPORT_QT_SHARED IMPORT WIN32
+			#endif
         #endif // _WIN32
 
         #ifdef _LINUX
             #define SUPPORT_QT_DLL_EXPORT
-			#warning SUPPORT_QT_SHARED IMPORT LINUX
+			#ifdef DEBUG_EXPORT_SYMBOLS
+				#warning SUPPORT_QT_SHARED IMPORT LINUX
+			#endif
         #endif // _LINUX
 	#endif // BUILD_SUPPORT_QT_DLL
 #endif
