149f3f7e352b8aad0084acc11334374f68da66b4
skeetor <sparhawk@gmx.at>
2015-10-02 14:13:44 +0200

    sizeof_member added

--- support_lib/support/support_defs.h
+++ support_lib/support/support_defs.h
@@ -14,4 +14,30 @@
 #define __STR2__(x) #x
 #define __DEFINE_TO_STRING(x) __STR2__(x)
 
+/**
+ * Macro for generating default member variables with getters and setters.
+ *
+ * BASE_MEMBER_SCOPED allows for each component to set the visibillity
+ * seperately.
+ * BASE_MEMBER is a shortcut where the variable is always private and the
+ * getters and setters are at the defined visibillity.
+ *
+ */
+#define BASE_MEMBER_SCOPED(getter_scope, setter_scope, member_scope, name, type) \
+	getter_scope: \
+	inline type get##name(void) const { return m##name;} \
+	setter_scope: \
+	inline void set##name(type name) { m##name = name; } \
+	member_scope:\
+	type m##name;
+
+#define BASE_MEMBER(scope, name, type) BASE_MEMBER_SCOPED(scope, scope, private, name, type)
+
+/**
+ * sizeof struct/class member.
+ * Example: sizeof_member(mystruct, membername)
+ */
+#define sizeof_member(type, member) sizeof(((type *)0)->member)
+
+
 #endif // __SUPPORT_DEFS_H__
